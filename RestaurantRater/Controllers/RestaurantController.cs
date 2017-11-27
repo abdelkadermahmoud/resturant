b03c211aea8597ab0f9a95cab0c7bd327e87e798
AJ Hephner <ajhephner178@gmail.com>
2017-11-27 15:05:44 -0500

    mod 5 done

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -13,7 +13,34 @@ namespace RestaurantRater.Controllers
         // GET: Restaurant
         public ActionResult Index()
         {
+
+            //Here return the view and find it at db Resturants then make it a list
             return View(db.Resturants.ToList());
         }
+        //GET: Resturant/Create
+        //Create() calls create view page
+        public ActionResult Create()
+        {
+            //return View() - show view
+            return View();
+        }
+
+        //POST: Resturant to our db
+        //passes data from forms 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ResturantID,Name")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Resturants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
     }
+
+   
 }
