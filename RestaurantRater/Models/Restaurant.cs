f721c9c3ef9b63c67eca6c33187c842e47f37ceb
AJ Hephner <ajhephner178@gmail.com>
2017-11-27 13:32:28 -0500

    section 2 done

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+
+        //properties
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+
+    //DbContext is a string to our DB 
+    //Handles relationship w/DB including querying 
+    public class RestaurantDBContext : DbContext
+    {
+        public DbSet<Restaurant> Resturants { get; set; }
+    }
+}
